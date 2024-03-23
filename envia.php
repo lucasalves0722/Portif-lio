ec9255d5f01fc89801e30b36abf25790ac95f56f
lucasalves0722 <lucasalves098messias@gmail.com>
2024-03-23 19:12:44 -0300

    commit php

--- envia.php
+++ envia.php
@@ -1,27 +1,21 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Coleta os dados do formulário
+
     $nome = $_POST['nome'];
     $email = $_POST['email'];
     $celular = $_POST['celular'];
     $mensagem = $_POST['mensagem'];
 
-    // Aqui você pode adicionar o código para enviar um e-mail com os dados ou armazená-los em um banco de dados
+    
+    $para = "lucasalves098messias@gemail.com";
+    $assunto = "Formulário de Contato - Portifólio";
 
-    // Exemplo de envio de e-mail
-    $destino = "lucasalves098messias@gemail.com";
-    $assunto = "Formulário de Contato";
-    $corpo_email = "Nome: $nome\n";
-    $corpo_email .= "E-mail: $email\n";
-    $corpo_email .= "Celular: $celular\n";
-    $corpo_email .= "Mensagem: $mensagem\n";
-    $headers = "From: 149004587+lucasalves0722@users.noreply.github.com.\n";
+    $corpo = "Nome: $nome."\n"."E-mail: $email."\n"."Celular: $celular."\n"."Mensagem: $mensagem;
 
-    // Envia o e-mail
-    if (mail($destino, $assunto, $corpo_email, $headers)) {
+    $cabeca = "From: 149004587+lucasalves0722@users.noreply.github.com"."\n"."Reply-to: ".$email."\n"."X-Mailer:PHP/".pgpversion();
+
+    if (mail($para, $assunto, $corpo, $cabeca)) {
         echo "E-mail enviado com sucesso!";
     } else {
         echo "Erro ao enviar o e-mail.";
     }
-}
 ?>
